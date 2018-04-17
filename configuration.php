f22ea4346db98fe392751332576050228c65e3a5
Johannes Meyer <johannes.meyer@icinga.com>
2018-04-17 14:40:46 +0200

    Introduce view for the audit log
    refs #1

--- configuration.php
+++ configuration.php
@@ -7,3 +7,19 @@ $this->provideConfigTab('config', [
     'url'   => 'config',
     'title' => $this->translate('Configuration')
 ]);
+
+$this->providePermission('audit/log', $this->translate('Allow access to the audit log'));
+
+try {
+    if ($this->getConfig()->get('log', 'type') === 'file') {
+        $section = $this->menuSection(N_('System'));
+        $section->add(N_('Audit Log'), [
+            'permission'    => 'audit/log',
+            'url'           => 'audit/log',
+            'icon'          => 'eye',
+            'priority'      => 910
+        ]);
+    }
+} catch (Exception $e) {
+    // This pops up again sooner or later anyway..
+}
