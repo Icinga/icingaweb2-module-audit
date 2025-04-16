b085f638f3024b99877fb52f7ee97211e275135d
Johannes Meyer <johannes.meyer@icinga.com>
2025-04-16 09:33:38 +0200

    Drop legacy view scripts

--- application/controllers/ConfigController.php
+++ application/controllers/ConfigController.php
@@ -6,18 +6,28 @@ namespace Icinga\Module\Audit\Controllers;
 
 use Icinga\Web\Controller;
 use Icinga\Module\Audit\Forms\Config\AuditLogConfigForm;
+use ipl\Html\HtmlString;
+use ipl\Web\Compat\CompatController;
 
-class ConfigController extends Controller
+class ConfigController extends CompatController
 {
+    public function init()
+    {
+        foreach ($this->Module()->getConfigTabs()->getTabs() as $tab) {
+            $this->tabs->add($tab->getName(), $tab);
+        }
+    }
+
     public function indexAction(): void
     {
+        $this->getTabs()->activate('config');
+
         $form = new AuditLogConfigForm();
         $form->setIniConfig($this->Config());
         $form->setSubmitLabel($this->translate('Save Configuration'));
 
         $form->handleRequest();
 
-        $this->view->form = $form;
-        $this->view->tabs = $this->Module()->getConfigTabs()->activate('config');
+        $this->addContent(new HtmlString((string) $form));
     }
 }
